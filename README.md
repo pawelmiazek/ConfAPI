de2a96707d96442d830dbd182b658a12935ad5dd
Paweł Miążek <p.miazek@merixstudio.com>
2021-10-24 20:22:13 +0200

    Update README.md and API schema

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-API for configuration json files
+# ConfAPI
+
+## Technology
+
+- Django
+- django rest framework
+- PostgreSQL
+- nested_lookup
+
+The reason of chosen technology is that they have good community support and official documentation. Additionaly these technologies are used in many companies around the world.
+
+## Prequesites
+
+- Due to compose file format 3.8 (Required Docker Engine release 19.03.0+)
+
+## Development Setup
+
+1. Create your own version of `env` by simply copying contents of `env.example`.
+
+2. In root directory run `docker-compose up` to build the app.
+
+## Task 1
+
+1. Run `make manage create_configurator {base_file} {params_file}` to create configurator json files.
+
+## Task 2
+
+1. Endpoint for generating configurator json files: `/v1/configurator/`.
+
+2. API Schema: `/v1/docs/`.
--- src/api/views.py
+++ src/api/views.py
@@ -2,14 +2,32 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from .serializers import ConfiguratorSerializer
+
+
+response_schema_dict = {
+    "200": openapi.Response(
+        description="200",
+        examples={
+            "application/json": {
+                "results": [],
+            }
+        }
+    )
+}
 
 
 class ConfiguratorView(APIView):
     permission_classes = (AllowAny,)
     serializer_class = ConfiguratorSerializer
 
+    @swagger_auto_schema(
+        request_body=ConfiguratorSerializer,
+        responses=response_schema_dict
+    )
     def post(self, request, *args, **kwargs):
         serializer = ConfiguratorSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
